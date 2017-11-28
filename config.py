72d2934eeab92ca858cec71c56c6c7e3e9040893
whimo <me@whimo.me>
2017-11-28 20:44:08 +0300

    Alerts added

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 basedir = '/sys/bus/w1/devices/'
 api_key = '369630933:AAHcWi60TqNIgUZqH0peJwvAnykotgepCYs'
 users_file = 'users.txt'
+sensors_file = 'sensors.txt'
--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import os
 from functools import wraps
 import config
 import glob
+from threading import Thread
 import telebot
+import time
 
 
 bot = telebot.TeleBot(config.api_key)
 users = []
+sensors = {}
+critical_temp = 0
+secret_key = ''
 
 
 def auth_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
-        if args[0].from_user.username not in users:
-            print('user', args[0].from_user.username)
-            print(users)
+        if args[0].from_user.username not in users and args[0].chat.id not in users:
             bot.send_message(args[0].chat.id, 'Auth required')
             return -1
 
@@ -30,22 +33,46 @@ def read():
             lines = f.readlines()
 
             if lines[0].strip()[-3:] != 'YES':
-                ret[slave] = 'HARDWARE_ERROR'
+                ret[slave] = 'HARDWARE ERROR'
 
             else:
                 pos = lines[1].find('t=')
-                ret[slave] = float(lines[1][pos + 2:]) / 1000.0 if pos != -1 else 'READ_ERROR'
+                sensor_id = slave.split('/')[-2]
+                ret[sensors[sensor_id] if sensor_id in sensors else sensor_id] =\
+                    round(float(lines[1][pos + 2:]) / 1000.0, 1) if pos != -1 else 'READ ERROR'
 
     return ret if ret else {'Error': 'no sensors found'}
 
 
 def main():
+    global users
+    global sensors
+    global critical_temp
+    global secret_key
+
     os.system('sudo modprobe w1-gpio')
     os.system('sudo modprobe w1-therm')
 
     with open(config.users_file, 'r') as f:
         for user in f.readlines():
-            users.append(user.replace('\n', ''))
+            if 'SECRET_KEY' in user:
+                secret_key = int(user.split(' ')[1])
+            else:
+                try:
+                    users.append(int(user.replace('\n', '')))
+                except ValueError:
+                    users.append(user.replace('\n', ''))
+
+    with open(config.sensors_file, 'r') as f:
+        for sensor_info in f.readlines():
+            data = sensor_info.split(' ')
+            if not data:
+                continue
+
+            if data[0] == 'CRITICAL_TEMP':
+                critical_temp = int(data[1])
+
+            sensors[data[0]] = data[1]
 
 
 if __name__ == '__main__':
@@ -58,11 +85,51 @@ def start(message):
     bot.send_message(message.chat.id, '/status or /s for status')
 
 
+@bot.message_handler(commands=['auth'])
+def auth(message):
+    if message.from_user.username not in users and message.chat.id not in users:
+        if secret_key in message.text:
+            with open(config.users_file, 'w') as f:
+                f.write(str(message.chat.id) + '\n')
+
+            bot.send_message(message.chat.id, 'Success! /status or /s for status')
+
+        else:
+            bot.send_message(message.chat.id, 'Bad key, fuck off')
+
+    else:
+        bot.send_message(message.chat.id, 'Already authenticated')
+
+
 @bot.message_handler(commands=['status', 's'])
 @auth_required
 def status(message):
     info = read()
-    bot.send_message(message.chat.id, '\n'.join([key + ': ' + str(info[key]) for key in info.keys()]))
+    bot.send_message(message.chat.id, '\n'.join([key + ': ' + str(info[key]) + 'C' for key in info.keys()]))
 
 
-bot.polling()
+thread = Thread(target=bot.polling)
+thread.setDaemon(True)
+thread.start()
+
+criticals = {}
+
+while True:
+    for sensor in criticals:
+        if criticals[sensor] >= 6:
+            for user in users:
+                try:
+                    bot.send_message(int(user),
+                                     'CRITICAL TEMPERATURE on sensor {}: {}'.format(sensor, read()[sensor]))
+                except ValueError:
+                    pass
+        criticals[sensor] += 1
+    info = read()
+    for sensor in info:
+        if info[sensor] >= critical_temp and sensor not in criticals:
+            criticals[sensor] = 1
+
+        if info[sensor] < critical_temp and sensor in criticals:
+            criticals = criticals.pop(sensor)
+
+    time.sleep(30)
